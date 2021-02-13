b486b8acbcc81db4f2b2290de8c22181104e8603
Peter-Massarello <pmassarello@gmail.com>
2021-02-12 18:16:34 -0800

    Initial Layout done for first activity, clear function created and working

--- app/src/main/java/com/example/androidproject1/MainActivity.kt
+++ app/src/main/java/com/example/androidproject1/MainActivity.kt
@@ -2,10 +2,42 @@ package com.example.androidproject1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(){
+    
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val adapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.year_array,
+                android.R.layout.simple_spinner_item
+        )
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = adapter
+
+        clearButton.setOnClickListener(clearButtonClicked)
+        //spinner.onItemSelectedListener(spinnerItemSelected)
+
+    }
+
+    private val clearButtonClicked = View.OnClickListener {
+        clearAllForms() // Once clicked, goes to clear form function
+    }
+
+    private fun clearAllForms(){
+        /*
+         * Needs to reset spinner input as well
+         */
+        apr_input.getText().clear()
+        escrow_input.getText().clear()
+        loan_input.getText().clear()
     }
 }
+
---
+++ app/src/main/java/com/example/androidproject1/SpinnerClass.kt
@@ -0,0 +1,21 @@
+package com.example.androidproject1
+
+import android.content.Context
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import kotlinx.android.synthetic.main.activity_main.*
+
+class SpinnerClass(context: Context) {
+
+    /*init{
+        val adapter = ArrayAdapter.createFromResource(
+                context,
+                R.array.year_array,
+                android.R.layout.simple_spinner_item
+        )
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = adapter
+    }*/
+
+
+}
