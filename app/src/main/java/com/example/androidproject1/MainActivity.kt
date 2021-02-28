4fb4c223f8f88f055d1d67cfd4ee0c56b62de8f2
Peter-Massarello <pmassarello@gmail.com>
2021-02-27 20:51:35 -0800

    Working first activity, adding functionality to second

--- app/src/main/java/com/example/androidproject1/MainActivity.kt
+++ app/src/main/java/com/example/androidproject1/MainActivity.kt
@@ -2,20 +2,34 @@ package com.example.androidproject1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.EditText
+import android.widget.TextView
 import androidx.annotation.IdRes
 import androidx.core.widget.doAfterTextChanged
+import androidx.lifecycle.ViewModelProvider
+import com.example.androidproject1.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.concurrent.TimeoutException
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
     
+    lateinit var model: MainViewModel
+    lateinit var binding: ActivityMainBinding
+    var aprFlag = false
+    var escrowFlag = false
+    var loanFlag = false
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(this.layoutInflater)
+        setContentView(binding.root)
+
+        var submitFlag: Boolean = false
 
         val adapter = ArrayAdapter.createFromResource(
                 this,
@@ -24,52 +38,112 @@ class MainActivity : AppCompatActivity() {
         )
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         spinner.adapter = adapter
-        apr_input.doAfterTextChanged {
-            validateText(3, R.id.apr_input);
+        val provider = ViewModelProvider(this, ViewModelProvider.AndroidViewModelFactory.getInstance(application))
+        model = provider.get(MainViewModel::class.java)
+
+        binding.aprInput.doAfterTextChanged {
+            aprFlag = validateAPRText(binding.aprInput)
+            submitFlagCheck()
         }
+        binding.escrowInput.doAfterTextChanged {
+            escrowFlag = validateEscrowText(binding.escrowInput)
+            submitFlagCheck()
+        }
+        binding.loanInput.doAfterTextChanged {
+            loanFlag = validateLoanText(binding.loanInput)
+            submitFlagCheck()
+        }
+
+        submitButton.setOnClickListener(submitButtonClicked)
+
         clearButton.setOnClickListener(clearButtonClicked)
-
+        spinner.onItemSelectedListener = this
     }
 
-    private val clearButtonClicked = View.OnClickListener {
-        clearAllForms() // Once clicked, goes to clear form function
+    private val clearButtonClicked = View.OnClickListener { // Once clicked, goes to clear form function
+        clearAllForms()
     }
 
-    private fun clearAllForms(){
-        /*
-         * Needs to reset spinner input as well
-         */
-        apr_input.getText().clear()
-        escrow_input.getText().clear()
-        loan_input.getText().clear()
+    private val submitButtonClicked = View.OnClickListener { // Once clicked, goes to clear form function
+        passToNextAct()
     }
 
-    private fun validateText(decimalCheck: Int, @IdRes viewId: Int){
-        //val temp: EditText = findViewById(R.id.viewId)
-        val input = apr_input.text.toString()
-        val wholeNumbers: Int = input.indexOf('.')
-        val decimalCount: Int = input.length - wholeNumbers - 1
+    private fun passToNextAct(){
+        Log.e("Item", "${aprFlag}")
+    }
 
-        if (decimalCount > decimalCheck){
-            apr_input.error = "Can only be to $decimalCheck decimal places."
+    private fun submitFlagCheck(){
+        binding.submitButton.isEnabled = aprFlag && escrowFlag && loanFlag
+    }
+
+    private fun clearAllForms(){ //Resets all Input Forms
+        spinner.setSelection(0)
+        apr_input.text.clear()
+        escrow_input.text.clear()
+        loan_input.text.clear()
+    }
+
+   private fun validateAPRText(textView: TextView): Boolean{
+       val input = textView.text.toString()
+
+       if (input.length < 1) {
+           apr_input.error = "Field cannot be blank."
+           return false
+       }
+       val pattern = "[0-9]+(\\.[0-9]?[0-9]?[0-9]?)".toRegex()
+       if(pattern.matches(input)){
+           return true
+       }
+       else {
+           apr_input.error = "Must have up to 3 decimal places"
+           return false
+       }
+       return false
+    }
+
+    private fun validateEscrowText(textView: TextView): Boolean{
+        val input = textView.text.toString()
+
+        if (input.length < 1) {
+            escrow_input.error = "Field cannot be blank."
+            return false
         }
 
+        val pattern = "[0-9]+(\\.[0-9]?[0-9]?)".toRegex()
+        if(pattern.matches(input)){
+            return true
+        }
+        else {
+            escrow_input.error = "Must have up to 3 decimal places"
+            return false
+        }
+        return false
+    }
+    private fun validateLoanText(textView: TextView): Boolean{
+       val input = textView.text.toString()
+
+        if (input.length < 1) {
+            loan_input.error = "Field cannot be blank."
+            return false
+        }
+        val pattern = "\\d*".toRegex()
+        if(pattern.matches(input)){
+            return true
+        }
+        else {
+            loan_input.error = ""
+            return false
+        }
+    }
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+        TODO("Not yet implemented")
     }
 
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
 
-
-    /*private fun EditText.onTextChange(onTextChange: (String) -> Unit){
-        this.addTextChangedListener(object: TextWatcher{
-            override fun afterTextChanged(s: Editable?) {
-                onTextChange.invoke(s.toString())
-            }
-
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
-        })
-
-    }*/
+        Log.e("Item", "${spinner.selectedItem.toString().toInt()}")
+    }
 
 }
+
 
---
+++ app/src/main/java/com/example/androidproject1/MainViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.androidproject1
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class MainViewModel constructor(application: Application): AndroidViewModel(application) {
+
+}
